8499b2581ca151b174d138f420b77eae0ceaf016
AmirHossein Habibvand <ahhabibvand@gmail.com>
2020-08-12 16:42:19 +0430

    Separate activities

--- app/src/main/java/com/example/escaperoomcontroller/MainActivity.java
+++ app/src/main/java/com/example/escaperoomcontroller/MainActivity.java
@@ -1,98 +1,27 @@
 package com.example.escaperoomcontroller;
 
-import android.content.Context;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
-import android.os.Handler;
-import android.widget.TextView;
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-import java.io.IOException;
-import java.net.*;
-
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
-
-    private SensorManager sensorManager;
-    private TextView textView;
+public class MainActivity extends AppCompatActivity {
+    static final String EXTRA_IP = "EXTRA_IP";
+    static final String EXTRA_PORT = "EXTRA_PORT";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-
         setContentView(R.layout.activity_main);
-
-        textView = findViewById(R.id.textView);
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        sensorManager.registerListener(this,
-//                sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY),
-                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
-                SensorManager.SENSOR_DELAY_GAME);
-    }
+    public void startGame(View view) {
+        Intent i = new Intent(this, GameActivity.class);
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        sensorManager.unregisterListener(this);
-    }
+        i.putExtra(EXTRA_IP, ((EditText) findViewById(R.id.server_ip)).getText().toString());
+        i.putExtra(EXTRA_PORT, ((EditText) findViewById(R.id.server_port)).getText().toString());;
 
-    @Override
-    public void onSensorChanged(SensorEvent sensorEvent) {
-        if (sensorEvent.accuracy == SensorManager.SENSOR_STATUS_UNRELIABLE) {
-            return;
-        }
-
-        float[] values = sensorEvent.values;
-        final String msg = String.format("%f %f %f %d\n", -1 * values[0], values[1], values[2], sensorEvent.timestamp);
-        textView.setText(msg);
-        sendMessage(msg);
-    }
-
-    private void sendMessage(final String message) {
-
-        final Handler handler = new Handler();
-        Thread thread = new Thread(new Runnable() {
-            String stringData;
-
-            @Override
-            public void run() {
-                try (DatagramSocket datagramSocket = new DatagramSocket()) {
-                    // IP Address below is the IP address of that Device where server socket is opened.
-                    final InetAddress serverAddr = InetAddress.getByName("192.168.1.9");
-                    final DatagramPacket datagramPacket = new DatagramPacket(message.getBytes(), message.length(), serverAddr, 9001);
-                    datagramSocket.send(datagramPacket);
-
-//                    byte[] lMsg = new byte[1000];
-//                    final DatagramPacket resp = new DatagramPacket(lMsg, lMsg.length);
-//                    datagramSocket.receive(resp);
-//                    stringData = new String(lMsg, 0, resp.getLength());
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        System.out.println(message);
-                    }
-                });
-            }
-        });
-
-        thread.start();
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int i) {
-
+        startActivity(i);
     }
 }
